17703ee463f49395bec00b44b45b8143cc8e9c67
tomasjpn <tomasphammc@gmail.com>
2024-10-11 22:30:04 +0200

    Accessing Fastify Server from React App.tsx

--- server/index.ts
+++ server/index.ts
@@ -7,8 +7,20 @@ const fastify = Fastify({ logger: true });
 
 // Register the CORS Plug-in
 fastify.register(cors, {
-  // Only local host domains as origin
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: (origin, callback) => {
+    const localhost = [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'http://127.0.0.1:5173',
+      'http://127.0.0.1:5174',
+    ];
+    // Ensures, that the origin URL is included in the localhost-list
+    if (localhost.indexOf(origin as string) !== -1 || !origin) {
+      callback(null, true);
+      return;
+    }
+    callback(new Error('Not allowed'), false);
+  },
   // Methods accepted by the server
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
@@ -17,13 +29,19 @@ fastify.register(cors, {
 // GET-request from Endpoint "/v1/calc" -> defintion of the route on the local Fastify-Server
 fastify.get('/v1/calc', async (request, reply): Promise<void> => {
   // Reads the URL-parameters of the request f.e.: "/v1/calc?expression=2+2"
-  const { expression } = request.query as { expression: string };
+  const { expression, x } = request.query as { expression: string; x: string };
+
+  // If value is missing in the expression Input
+  if (!expression) {
+    return reply.status(400).send({ error: 'Expression is required!' });
+  }
 
   try {
     // Fetching the Math-Func-Parser API
     const response = await axios.get('https://math.oglimmer.de/v1/calc', {
-      params: { expression },
+      params: { expression, x },
     });
+    console.log(`API response: ${JSON.stringify(response.data)}`);
     return response.data;
   } catch (err) {
     fastify.log.error(err);
