35277cffa71de69bad8f6b28d3cb926fa8e52c9f
Pham, Tomas <tomas.pham@materna.group>
2024-10-14 14:25:38 +0200

    Fastify Server API expanded with resolve, tokenize, ast

--- server/index.ts
+++ server/index.ts
@@ -27,25 +27,37 @@ fastify.register(cors, {
 });
 
 // GET-request from Endpoint "/v1/calc" -> defintion of the route on the local Fastify-Server
-fastify.get('/v1/calc', async (request, reply): Promise<void> => {
+fastify.get('/v1/:operation', async (request, reply): Promise<void> => {
+  // Operation for different options: 'resolve', 'tokenize', 'ast', 'calc'
+  const { operation } = request.params as { operation: string };
   // Reads the URL-parameters of the request f.e.: "/v1/calc?expression=2+2"
   const { expression, x } = request.query as { expression: string; x: string };
 
-  // If value is missing in the expression Input
+  // If operation is not valid
+  if (!['resolve', 'tokenize', 'ast', 'calc'].includes(operation)) {
+    return reply.status(400).send({ error: 'Invalid operation!' });
+  }
+
   if (!expression) {
+    // If value is missing in the expression Input
     return reply.status(400).send({ error: 'Expression is required!' });
   }
 
   try {
     // Fetching the Math-Func-Parser API
-    const response = await axios.get('https://math.oglimmer.de/v1/calc', {
-      params: { expression, x },
-    });
+    const response = await axios.get(
+      `https://math.oglimmer.de/v1/${operation}`,
+      {
+        params: { expression, x },
+      }
+    );
     console.log(`API response: ${JSON.stringify(response.data)}`);
     return response.data;
   } catch (err) {
     fastify.log.error(err);
-    return reply.status(500).send({ error: 'Error at calculating' });
+    return reply
+      .status(500)
+      .send({ error: `Error at ${operation} operation!` });
   }
 });
 
--- src/components/CalculatorForm.tsx
+++ src/components/CalculatorForm.tsx
@@ -1,11 +1,14 @@
 interface CalcInput {
+  operation: string;
   expression: string;
   x?: string;
 }
 
 interface CalcFormProperties {
   input: CalcInput;
-  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleInputChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
   handleCalc: () => void;
   calcResult: string;
 }
@@ -35,7 +38,18 @@ export const CalculatorForm: React.FC<CalcFormProperties> = ({
           onChange={handleInputChange}
           placeholder="x"
         />
-        <button onClick={handleCalc}>Calculate</button>
+        <select
+          name="operation"
+          value={input.operation}
+          onChange={handleInputChange}
+        >
+          <option value="resolve">resolve</option>
+          <option value="tokenize">tokenize</option>
+          <option value="ast">ast</option>
+          <option value="calc">calculate</option>
+        </select>
+        <button onClick={handleCalc}>Enter</button>
+
         <p>{calcResult}</p>
       </div>
     </div>
