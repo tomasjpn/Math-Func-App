143678d9b31194788f3b7c9c2c0ebfe5c9d00062
Pham, Tomas <tomas.pham@materna.group>
2024-10-21 17:27:29 +0200

    CSS adjustment

--- src/components/DisplayCalculationHistory.tsx
+++ src/components/DisplayCalculationHistory.tsx
@@ -29,13 +29,93 @@ const DisplayCalculationHistory: React.FC = () => {
   if (loadingScreen) return <div>Loading...</div>;
   if (errorScreen) return <div>Error: {errorScreen}</div>;
 
+  const limitedCalculationHistory = calculationHistory.slice(0, 5);
+
   return (
-    <div>
-      <h2>Calculation History</h2>
-      <ul>
-        {calculationHistory.map((record) => (
-          <li key={record.id}>
-            {record.expression} ({record.operation})
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <h2>Calculation History : </h2>
+      <ul
+        style={{
+          listStyleType: 'none',
+          padding: 0,
+          width: '100%',
+          maxWidth: '600px',
+        }}
+      >
+        {limitedCalculationHistory.map((record, index) => (
+          <li
+            key={record.id}
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              backgroundColor: '#dfe9ff',
+              marginBottom: '10px',
+              padding: '8px 15px',
+              borderRadius: '15px',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                margin: 0,
+                padding: 0,
+                fontSize: '18px',
+                color: '#1152c4',
+              }}
+            >
+              <h4 style={{ margin: '0 50px 0 0' }}>{index + 1}.</h4>
+              <p style={{ fontWeight: '600', margin: 0 }}>
+                {record.expression} ({record.operation})
+              </p>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <button
+                style={{
+                  backgroundColor: '#3f86ff',
+                  marginLeft: '10px',
+                  fontWeight: 'bold',
+                  padding: '6px 10px',
+                  borderRadius: '10px',
+                  color: '#fff',
+                }}
+              >
+                Select
+              </button>
+              <button
+                style={{
+                  backgroundColor: '#3f86ff',
+                  marginLeft: '5px',
+                  fontWeight: 'bold',
+                  padding: '6px 10px',
+                  borderRadius: '10px',
+                  color: '#fff',
+                }}
+              >
+                Edit
+              </button>
+              <button
+                style={{
+                  backgroundColor: '#3f86ff',
+                  marginLeft: '5px',
+                  fontWeight: 'bold',
+                  padding: '6px 10px',
+                  borderRadius: '10px',
+                  color: '#fff',
+                }}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
